3d07e78bbcf1702999161227c570e15255e29d45
cah4opan4o <volodin.sasha2004@yandex.ru>
2025-04-11 17:38:06 +0700

    complete fix modulator and demodulator + add gitignore

--- rx/ofdm_demodulator.m
+++ rx/ofdm_demodulator.m
@@ -3,36 +3,33 @@ function rx_ofdmSymbols = ofdm_demodulator(AWGNbits)
     indexNrs = getappdata(0,'indexNrs');
     Nz = getappdata(0,'Nz');
     PilotValue = getappdata(0,'PilotValue');
-    C = getappdata(0,'C');
     T = getappdata(0,'T');
+    Ncp = getappdata(0,'Ncp');
 
-    AWGNbits = AWGNbits(floor(T * length(AWGNbits)) : end); % Удалил защитный интервал
+    % Удаляем циклический префикс
+    AWGNbits = AWGNbits( Ncp + 1 : end);
+
+    % Переход в частотную область
     dpf = fft(AWGNbits);
-    disp(length(dpf));
-    signal_without_zeros = dpf(Nz + 1 : end - Nz); % Удалил нулевые поднесущие
-    disp(length(signal_without_zeros))
-    Rrx = PilotValue;
-    Rtx = signal_without_zeros(indexNrs);
-    H = Rrx ./ Rtx;
-    signal_correct = signal_without_zeros ./ H;
+
+    % Удаляем нулевые поднесущие
+    signal_without_zeros = dpf(Nz + 1 : end - Nz);
+
+    % Извлекаем пилоты
+    Rrx = PilotValue; % что было отправлено
+    Rtx = signal_without_zeros(indexNrs); % что пришло
+
+    % Оценка канала
+    H_est = Rrx ./ Rtx;
+
+    % Интерполяция
+    all_indices = 1:length(signal_without_zeros);
+    H_interp = interp1(indexNrs, H_est, all_indices, 'linear', 'extrap');
+
+    % Коррекция канала
+    signal_correct = signal_without_zeros ./ H_interp;
+
+    % Удаление пилотов
     signal_correct(indexNrs) = [];
     rx_ofdmSymbols = signal_correct;
-end                         
-
-% function rx_ofdmSymbols = ofdm_demodulator(AWGNbits)
-%     indexNrs = getappdata(0, 'indexNrs');   % Индексы опорных поднесущих
-%     Nz = getappdata(0, 'Nz');               % Длина защитных интервалов
-%     PilotValue = getappdata(0, 'PilotValue'); % Опорные символы
-%     C = getappdata(0, 'C');                 % Число поднесущих
-%     signal_no_prefix = AWGNbits(Nz + 1 : end); 
-%     spectrum = fft(signal_no_prefix);
-%     signal_without_zeros = spectrum(Nz + 1 : end - Nz);
-%     Rrx = signal_without_zeros(indexNrs);  
-%     Rtx = PilotValue;
-%     H = Rrx ./ Rtx;
-%     subcarrier_indices = linspace(1, length(signal_without_zeros), length(H));
-%     H_interp = interp1(subcarrier_indices, H, 1:length(signal_without_zeros), 'linear', 'extrap');
-%     CEQ = signal_without_zeros ./ H_interp;
-%     data_subcarriers = setdiff(1:length(CEQ), indexNrs);  % Убираем опорные поднесущие
-%     rx_ofdmSymbols = CEQ(data_subcarriers);
-% end
+end
--- tx/ofdm_modulator.m
+++ tx/ofdm_modulator.m
@@ -1,22 +1,40 @@
 function ofdmSymbols = ofdm_modulator(qpskSymbols, dRS, T)
-    Nqpsk = numel(qpskSymbols); % Используем numel() для получения числа элементов
-    Nrs = floor(Nqpsk / dRS); % Количество опорных сигналов
-    C = 0.25;
-    Nz = round(C * (Nrs + Nqpsk)); % Количество нулевых поднесущих
+    % Получаем количество QPSK-символов
+    Nqpsk = numel(qpskSymbols); 
 
+    % Вычисляем количество пилотных символов (опорных сигналов)
+    Nrs = floor(Nqpsk / dRS); 
+
+    % Коэффициент для расчёта нулевых поднесущих (обычно 25% от общего числа)
+    C = 0.25;
+
+    % Расчёт количества нулевых поднесущих
+    Nz = round(C * (Nrs + Nqpsk)); 
+
+    % Создаём массив, в который будут вставлены как QPSK-символы, так и пилоты
     SignalWithPilots = zeros(1, Nqpsk + Nrs); 
 
+    % Счётчики для текущей позиции в qpskSymbols и числа вставленных пилотов
+    qpskCounter = 1;
+    nrsCounter = 0;
+
+    % Массив для хранения индексов, где вставлены пилотные символы
     indexNrs = [];
-    for i = 1:numel(SignalWithPilots)
-        if mod(i, dRS) == 0
-            SignalWithPilots(i) = 0.707 + 0.707i;
-            indexNrs = [indexNrs, i];
+
+    % Цикл по всем позициям в массиве с пилотами
+    for i = 1:length(SignalWithPilots)
+        % Если текущий индекс кратен dRS и ещё не вставили все пилоты
+        if mod(i, dRS) == 0 && nrsCounter < Nrs
+            SignalWithPilots(i) = 0.707 + 0.707i; % Вставляем пилот
+            indexNrs(end + 1) = i;                % Сохраняем индекс
+            nrsCounter = nrsCounter + 1;          % Увеличиваем счётчик пилотов
         else
-            qpskElement = qpskSymbols(1);
-            qpskSymbols(1) = []; % Удаляем первый элемент
-            SignalWithPilots(i) = qpskElement;
+            SignalWithPilots(i) = qpskSymbols(qpskCounter); % Вставляем QPSK-символ
+            qpskCounter = qpskCounter + 1;                  % Переходим к следующему символу
         end
     end
+
+    % Сохраняем важные параметры для демодулятора
     setappdata(0,'indexNrs',indexNrs);
     setappdata(0,'Nz',Nz);
     setappdata(0,'PilotValue',0.707 + 0.707i);
@@ -24,10 +42,20 @@ function ofdmSymbols = ofdm_modulator(qpskSymbols, dRS, T)
     setappdata(0,'T',T);
     setappdata(0,'dRS',dRS);
 
+    % Добавляем нулевые поднесущие по краям (паддинг)
     SignalWithZeroPadding = zeros(1, Nqpsk + Nrs + 2 * Nz);
     SignalWithZeroPadding(Nz + 1 : end - Nz) = SignalWithPilots;
 
+    % Преобразуем в частотную область (OFDM-модуляция)
     odpf = ifft(SignalWithZeroPadding);
-    output = [odpf(end - T * Nqpsk + 1 : end), odpf];
+
+    % Вычисляем длину защитного интервала (циклический префикс)
+    Ncp = round(T * length(odpf));
+    setappdata(0,'Ncp',Ncp);
+
+    % Добавляем циклический префикс (копируем последние Ncp символов в начало)
+    output = [odpf(end - Ncp + 1 : end), odpf];
+
+    % Возвращаем итоговый OFDM-сигнал
     ofdmSymbols = output;
 end
